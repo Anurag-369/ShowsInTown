ee0748456f2ab3289e7f231151694ea7b4eb928e
Anurag dubey <dubeyanurag340@gmail.com>
2026-02-05 22:40:35 +0530

    trying to coonect inngest

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,21 +1,26 @@
 import { Inngest } from "inngest";
-import User from "../models/user.js"; // Updated to correct filename
+import User from "../models/user.js";
 
-export const inngest = new Inngest({ id: "movie-ticket-booking" });
+export const inngest = new Inngest({ 
+  id: "movie-ticket-booking",
+  eventKey: process.env.INNGEST_EVENT_KEY, // Add this
+});
 
 // inngest function to save user data to db
 const syncUserCreation = inngest.createFunction(
     { id: 'sync-user-from-clerk' },
     { event: 'clerk/user.created' },
-    async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data;
-        const userData = {
-            _id: id,
-            email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
-            image: image_url
-        };
-        await User.create(userData);
+    async ({ event, step }) => {
+        return await step.run('save-user-to-db', async () => {
+            const { id, first_name, last_name, email_addresses, image_url } = event.data;
+            const userData = {
+                _id: id,
+                email: email_addresses[0].email_address,
+                name: first_name + ' ' + last_name,
+                image: image_url
+            };
+            return await User.create(userData);
+        });
     }
 );
 
@@ -23,9 +28,11 @@ const syncUserCreation = inngest.createFunction(
 const syncUserDeletion = inngest.createFunction(
     { id: 'delete-user-with-clerk' },
     { event: 'clerk/user.deleted' },
-    async ({ event }) => {
-        const { id } = event.data;
-        await User.findByIdAndDelete(id);
+    async ({ event, step }) => {
+        return await step.run('delete-user-from-db', async () => {
+            const { id } = event.data;
+            return await User.findByIdAndDelete(id);
+        });
     }
 );
 
@@ -33,15 +40,16 @@ const syncUserDeletion = inngest.createFunction(
 const syncUserUpdation = inngest.createFunction(
     { id: 'update-user-from-clerk' },
     { event: 'clerk/user.updated' },
-    async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data;
-        const userData = {
-            _id: id,
-            email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
-            image: image_url
-        };
-        await User.findByIdAndUpdate(id, userData);
+    async ({ event, step }) => {
+        return await step.run('update-user-in-db', async () => {
+            const { id, first_name, last_name, email_addresses, image_url } = event.data;
+            const userData = {
+                email: email_addresses[0].email_address,
+                name: first_name + ' ' + last_name,
+                image: image_url
+            };
+            return await User.findByIdAndUpdate(id, userData);
+        });
     }
 );
 
